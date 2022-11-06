2c13965fe22a15ac18eca5d25ae563d81b6a2bba
ptrszmczk <ptrszmczk@gmail.com>
2022-11-06 20:13:13 +0100

    Add project files.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DataTable _dataTable = new DataTable();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Result(string inputVal)
+        {
+            try
+            {
+                var result = Math.Round(Convert.ToDouble(_dataTable.Compute(inputVal.Replace(",", "."), "")), 2).ToString();
+
+                return Json(new { success = true, result });
+            }
+            catch(Exception exeption)
+            {
+                return Json(new { success = false, message = exeption.Message });
+            }
+        }
+    }
+}
